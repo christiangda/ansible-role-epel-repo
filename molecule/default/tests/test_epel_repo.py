bbfef1fed2e292be86f2de270eeb67d85cc3a094
Christian González Di Antonio <christiangda@gmail.com>
2019-05-04 17:46:38 +0200

    Fixed installation on Amazon Linux 1 Improved installation on Amazon Linux 2 Added molecule test writted with testinfra

--- molecule/default/tests/test_epel_repo.py
+++ molecule/default/tests/test_epel_repo.py
@@ -6,9 +6,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_epel_repo_file(host):
+    f = host.file('/etc/yum.repos.d/epel.repo')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_epel_release_is_installed(host):
+    pkg = host.package("epel-release")
+
+    assert pkg.is_installed
